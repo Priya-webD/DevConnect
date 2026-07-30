b29d8b3ba0f444c104bd3c9d56b6eefca93765c7
Priya-webD <panchal429meena@gmail.com>
2026-07-30 14:30:20 +0530

    day4: profile routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 connectDB();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({status: 'ok'});
