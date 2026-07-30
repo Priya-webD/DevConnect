807f98d1edffc46d5cf58c479dc91a010d38316f
Priya-webD <panchal429meena@gmail.com>
2026-07-30 14:04:00 +0530

    day3: login, jwt, protected /me route

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,8 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+//sign up
 
 export const signup = async (req, res) => {
   try {
@@ -24,3 +27,37 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+//login 
+
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
+        if(!user) return res.status(400).json({message: 'Invalid credentials'});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({message: 'Invalid credentials'});
+
+        const token = jwt.sign({id: user._id},
+             process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.status(200).json({
+            token, 
+            user: { id: user._id, name: user.name, email: user.email},
+        });
+
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+};
+
+export const getMe = async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.status(200).json({ user });
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/authController.js';
+import authMiddleware from '../middlewares/auth.js';
+import { signup, login, getMe } from '../controllers/authController.js';
 
 const router = express.Router();
 router.post('/signup', signup);
+router.post('/login', login);
+router.get('/me', authMiddleware, getMe);
 
 export default router;
